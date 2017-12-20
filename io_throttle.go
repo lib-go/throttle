a83f17fcdacd29c7c9bad5d0b7c2b317f86f3b3e
张成龙 <zhangchenglong@weiju.net>
2017-12-20 14:45:57 +0800

    sleep-discard-rw

--- io_throttle.go
+++ io_throttle.go
@@ -1,90 +1,163 @@
 package throttle
 
 import (
+	"fmt"
 	"io"
 	"sync"
 	"time"
 )
 
+const defaultNowCacheInterval = 100 * time.Millisecond
+
 var (
-	defaultRateControl *rateControl
+	defaultRateControl = &rateControl{
+		raters: make(map[string]*rater),
+	}
+	defaultNowCache    *nowCache
+	mutex              = &sync.Mutex{}
 )
 
+type nowCache struct {
+	Now      time.Time
+}
+
+func newNowCache() {
+	mutex.Lock()
+	if defaultNowCache == nil {
+		defaultNowCache = &nowCache{Now: time.Now()}
+		go defaultNowCache.foreverTick()
+	}
+	mutex.Unlock()
+}
+
+func (c *nowCache) foreverTick() {
+	for {
+		time.Sleep(defaultNowCacheInterval)
+		c.Now = time.Now()
+	}
+}
+
+// rateControl
 type rateControl struct {
 	sync.Mutex
-
-	now     time.Time
-	times   map[string]*time.Time
-	ticking bool
+	raters map[string]*rater
 }
 
-func NewRateControl() *rateControl {
-	return &rateControl{
-		now:   time.Now(),
-		times: make(map[string]*time.Time),
-	}
-}
-
-func (c *rateControl) foreverTick() {
-	for {
-		c.now = time.Now()
-		time.Sleep(time.Millisecond * 100)
-	}
-}
-
-func (c *rateControl) NewReader(rd io.Reader, rateLimitBPS int, key string) *reader {
-	if !c.ticking {
-		c.ticking = true
-		go c.foreverTick()
+func (c *rateControl) NewRater(rateLimitBPS int, key string) *rater {
+	// 作为reader的下次读取时间，同一个key的reader的recordTime指向同一个*time.Time
+	var raterRef *rater
+	makeRetRef := func() *rater {
+		r := &rater{
+			rateBPS:      rateLimitBPS,
+			estimateTime: new(time.Time),
+		}
+		*(r.estimateTime) = defaultNowCache.Now
+		return r
 	}
 
-	// 作为reader的下次读取时间，同一个key的reader的nextReadTime指向同一个*time.Time
-	var timeRef *time.Time
 	if key == "" {
-		timeRef = new(time.Time)
+		raterRef = makeRetRef()
 	} else {
 		c.Lock()
-		if timeRef = c.times[key]; timeRef == nil {
-			timeRef = new(time.Time)
-			c.times[key] = timeRef
+
+		if raterRef = c.raters[key]; raterRef == nil {
+			raterRef = makeRetRef()
+			c.raters[key] = raterRef
 		}
 		c.Unlock()
 	}
-	*timeRef = c.now
 
-	return &reader{
-		rc:           c,
-		rd:           rd,
-		rateBPS:      rateLimitBPS,
-		nextReadTime: timeRef,
+	return raterRef
+}
+
+// rater
+type rater struct {
+	sync.Mutex
+
+	rateBPS      int
+	estimateTime *time.Time
+}
+
+func (r *rater) UpdateEstimateTime(n int) {
+	if n > 0 && r.rateBPS > 0 {
+		//fmt.Println(n, "est:", time.Microsecond*time.Duration(float32(n)/float32(r.rateBPS)*1000*1000))
+		estimate := time.Microsecond * time.Duration(float32(n)/float32(r.rateBPS)*1000*1000)
+		*r.estimateTime = r.estimateTime.Add(estimate)
 	}
 }
 
-type reader struct {
-	rc *rateControl
+func (r *rater) WaitingDuration() time.Duration {
+	duration := r.estimateTime.Sub(defaultNowCache.Now)
 
-	rd           io.Reader
-	rateBPS      int // kbps
-	nextReadTime *time.Time
+	if duration > 0 {
+		return duration
+	} else {
+		return time.Duration(0)
+	}
 }
 
-func NewReader(reader io.Reader, rateLimitBPS int, key string) *reader {
-	if defaultRateControl == nil {
-		defaultRateControl = NewRateControl()
-	}
+// rw
+var ErrorDiscard = fmt.Errorf("[]byte discard")
 
-	return defaultRateControl.NewReader(reader, rateLimitBPS, key)
+type readWrite func(chunk []byte) (int, error)
+type raterRW func(chunk []byte, ope readWrite, rt *rater) (n int, e error)
+
+func raterSleep(chunk []byte, ope readWrite, rt *rater) (n int, e error) {
+	n, e = ope(chunk)
+	rt.UpdateEstimateTime(n)
+
+	if duration := rt.WaitingDuration(); duration > 0 {
+		time.Sleep(duration)
+	}
+	return n, e
 }
 
-func (r *reader) Read(chunk []byte) (n int, e error) {
-	n, e = r.rd.Read(chunk)
-
-	if r.rateBPS > 0 {
-		estimateReadDuration := time.Second * time.Duration(n) / time.Duration(r.rateBPS)
-		*(r.nextReadTime) = r.nextReadTime.Add(estimateReadDuration)
-		if r.nextReadTime.After(r.rc.now) {
-			time.Sleep(r.nextReadTime.Sub(r.rc.now))
-		}
+func raterDiscard(chunk []byte, ope readWrite, rt *rater) (n int, e error) {
+	// 先等再允许写入, 避免某个包写入时间过长, 后面包一直丢弃
+	if duration := rt.WaitingDuration(); duration > 0 {
+		n, e = 0, ErrorDiscard
+	} else {
+		n, e = ope(chunk)
+		rt.UpdateEstimateTime(n)
 	}
-	return
+	return n, e
+}
+
+type readWriter struct {
+	io.ReadWriter
+	ope readWrite
+	rrw raterRW
+	rt  *rater
+}
+
+func (r *readWriter) Read(chunk []byte) (int, error) {
+	return r.rrw(chunk, r.ope, r.rt)
+}
+
+func (r *readWriter) Write(chunk []byte) (int, error) {
+	return r.rrw(chunk, r.ope, r.rt)
+}
+
+func NewReaderWriter(ope readWrite, rateLimitBPS int, key string, rrw raterRW) *readWriter {
+	newNowCache()
+
+	rt := defaultRateControl.NewRater(rateLimitBPS, key)
+	return &readWriter{ope: ope, rrw: rrw, rt: rt}
+}
+
+// builder
+func NewSleepReader(rd io.Reader, rateLimitBPS int, key string) *readWriter {
+	return NewReaderWriter(rd.Read, rateLimitBPS, key, raterSleep)
+}
+
+func NewDiscardReader(rd io.Reader, rateLimitBPS int, key string) *readWriter {
+	return NewReaderWriter(rd.Read, rateLimitBPS, key, raterDiscard)
+}
+
+func NewSleepWriter(w io.Writer, rateLimitBPS int, key string) *readWriter {
+	return NewReaderWriter(w.Write, rateLimitBPS, key, raterSleep)
+}
+
+func NewDiscardWriter(w io.Writer, rateLimitBPS int, key string) *readWriter {
+	return NewReaderWriter(w.Write, rateLimitBPS, key, raterDiscard)
 }
--- io_throttle_test.go
+++ io_throttle_test.go
@@ -4,50 +4,233 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/stretchr/testify/assert"
-	"io/ioutil"
 	"sync"
 	"testing"
 	"time"
+	"io"
 )
 
-func Test_reader_single(t *testing.T) {
-	totalSize := 1024 * 1024
-	rateLimitBPS := 512 * 1024 // bps
-	var eta time.Duration = time.Second * time.Duration(totalSize) / time.Duration(rateLimitBPS)
+func Test_sleep_reader_shared_key(t *testing.T) {
+	var totalN int
+	var spend time.Duration
+	var eta time.Duration
+	var now time.Time
+	var readMaker func() io.Reader
+
+	totalSize := 1024 * 1024 * 2
+	rateLimitBPS := 512 * 1024
+	count := 3
 
 	blob := make([]byte, totalSize)
-	tr := NewReader(bytes.NewReader(blob), rateLimitBPS, "")
 
-	begin := time.Now()
-	ioutil.ReadAll(tr)
-	spend := time.Now().Sub(begin)
+	readMaker = func() io.Reader {
+		return NewSleepReader(bytes.NewReader(blob), rateLimitBPS, "Test_sleep_reader_shared_key")
+	}
 
-	fmt.Println(spend)
-	now := time.Now()
+	totalN, spend = multiRead(count, readMaker, rateLimitBPS, totalSize/count)
+
+	now = time.Now()
+	eta = time.Second * time.Duration(totalN) / time.Duration(rateLimitBPS)
+
+	fmt.Println("total", totalN, "spend", spend, "eta", eta, "eta-spend:", eta-spend)
 	assert.WithinDuration(t, now.Add(eta), now.Add(spend), eta/time.Duration(10), "偏差不超过10%")
 }
 
-func Test_reader_shared_key(t *testing.T) {
-	totalSize := 1024 * 1024
+func Test_sleep_writer_shared_key(t *testing.T) {
+	var totalN int
+	var spend time.Duration
+	var eta time.Duration
+	var now time.Time
+	var writeMaker func() io.Writer
+
+	totalSize := 1024 * 1024 * 2
 	rateLimitBPS := 512 * 1024
-	readerCount := 3
-	var eta time.Duration = time.Second * time.Duration(totalSize) / time.Duration(rateLimitBPS) * time.Duration(readerCount)
+	count := 3
 
-	var wg sync.WaitGroup
-	wg.Add(readerCount)
+	writeMaker = func() io.Writer {
+		return NewSleepWriter(&bytes.Buffer{}, rateLimitBPS, "Test_sleep_writer_shared_key")
+	}
 
+	totalN, spend = multiSend(count, writeMaker, rateLimitBPS, totalSize/count)
+
+	now = time.Now()
+	eta = time.Second * time.Duration(totalN) / time.Duration(rateLimitBPS)
+
+	fmt.Println("total", totalN, "spend", spend, "eta", eta, "eta-spend:", eta-spend)
+	assert.WithinDuration(t, now.Add(eta), now.Add(spend), eta/time.Duration(10), "偏差不超过10%")
+}
+
+func Test_discard_reader_shared_key(t *testing.T) {
+	var totalN int
+	var spend time.Duration
+	var eta time.Duration
+	var now time.Time
+	var speed int
+	var readMaker func() io.Reader
+
+	totalSize := 1024 * 1024 * 2
+	rateLimitBPS := 512 * 1024 // bps
+
+	blob := make([]byte, totalSize)
+	for i := 0; i < len(blob); i++ {
+		blob[i] = byte(i)
+	}
+	count := 3
+
+	// 低速
+	readMaker = func() io.Reader {
+		return NewDiscardReader(bytes.NewReader(blob), rateLimitBPS, "Test_discard_reader_shared_key_1")
+	}
+
+	speed = (rateLimitBPS - 128*1024) / count
+	totalN, spend = multiRead(count, readMaker, speed, totalSize/count)
+
+	now = time.Now()
+	eta = time.Second * time.Duration(totalN) / time.Duration(speed*count) // 期望以低速为准
+
+	fmt.Println("total", totalN, "spend", spend, "eta", eta, "eta-spend:", eta-spend)
+	assert.WithinDuration(t, now.Add(eta), now.Add(spend), eta/time.Duration(10), "偏差不超过10%")
+
+	// 高速
+	readMaker = func() io.Reader {
+		return NewDiscardReader(bytes.NewReader(blob), rateLimitBPS, "Test_discard_reader_shared_key_2")
+	}
+
+	speed = (rateLimitBPS + 128*1024) / count
+	totalN, spend = multiRead(count, readMaker, speed, totalSize/count)
+
+	now = time.Now()
+	eta = time.Second * time.Duration(totalN) / time.Duration(rateLimitBPS) // 期望以低速为准
+
+	fmt.Println("total", totalN, "spend", spend, "eta", eta, "eta-spend:", eta-spend)
+	assert.WithinDuration(t, now.Add(eta), now.Add(spend), eta/time.Duration(10), "偏差不超过10%")
+}
+
+func Test_discard_writer_shared_key(t *testing.T) {
+	var totalN int
+	var spend time.Duration
+	var eta time.Duration
+	var now time.Time
+	var writeMaker func() io.Writer
+	var speed int
+
+	totalSize := 1024 * 1024 * 2
+	rateLimitBPS := 512 * 1024 // bps
+	count := 3
+
+	//低速通过
+	writeMaker = func() io.Writer {
+		return NewDiscardWriter(&bytes.Buffer{}, rateLimitBPS, "Test_discard_writer_single_shared_key_1")
+	}
+
+	speed = (rateLimitBPS - 128*1024) / count
+	totalN, spend = multiSend(count, writeMaker, speed, totalSize/count)
+
+	now = time.Now()
+	eta = time.Second * time.Duration(totalN) / time.Duration(speed*count) // 期望以低速为准
+
+	fmt.Println("total", totalN, "spend", spend, "eta", eta, "eta-spend:", eta-spend)
+	assert.WithinDuration(t, now.Add(eta), now.Add(spend), eta/time.Duration(10), "偏差不超过10%")
+
+	// 高速丢弃
+	writeMaker = func() io.Writer {
+		return NewDiscardWriter(&bytes.Buffer{}, rateLimitBPS, "Test_discard_writer_single_shared_key_2")
+	}
+
+	speed = (rateLimitBPS + 128*1024) / count
+	totalN, spend = multiSend(count, writeMaker, speed, totalSize)
+
+	now = time.Now()
+	eta = time.Second * time.Duration(totalN) / time.Duration(rateLimitBPS)
+
+	fmt.Println("total", totalN, "spend", spend, "eta", eta, "eta-spend:", eta-spend)
+	assert.WithinDuration(t, now.Add(eta), now.Add(spend), eta/time.Duration(10), "偏差不超过10%")
+}
+
+func read(rd io.Reader, speed int, totalSize int) (int, time.Duration) {
+	sum := 0
+	readDuration := 100 * time.Millisecond
+	readSize := speed / int(time.Second/readDuration)
+	b := make([]byte, readSize)
 	begin := time.Now()
-	for i := 0; i < readerCount; i++ {
+	for sendN := 0; sendN < totalSize; sendN += readSize {
+		n, _ := rd.Read(b)
+		est := time.Millisecond * time.Duration(float64(sendN+readSize)/float64(speed)*1000)
+		duration := est - time.Now().Sub(begin)
+		//fmt.Println(sendN+readSize, speed, est, time.Now().Sub(begin), duration)
+		if duration > 0 {
+			time.Sleep(duration)
+		}
+		sum += n
+	}
+	spend := time.Now().Sub(begin)
+	//fmt.Println(sum, spend, float64(sum)/float64(speed))
+	return sum, spend
+}
+
+func multiRead(count int, readMaker func() io.Reader, speed int, totalSize int) (int, time.Duration) {
+	wg := sync.WaitGroup{}
+
+	wg.Add(count)
+
+	totalN := 0
+	begin := time.Now()
+	for i := 0; i < count; i++ {
 		go func() {
-			tr := NewReader(bytes.NewReader(make([]byte, totalSize)), rateLimitBPS, "shared-key")
-			ioutil.ReadAll(tr)
+			rd := readMaker()
+			// 确保原子操作
+			n, _ := read(rd, speed, totalSize)
+			totalN += n
 			wg.Done()
 		}()
 	}
 	wg.Wait()
 
 	spend := time.Now().Sub(begin)
-	fmt.Println(spend)
-	now := time.Now()
-	assert.WithinDuration(t, now.Add(eta), now.Add(spend), eta/time.Duration(10), "偏差不超过10%")
+	return totalN, spend
+}
+
+func send(w io.Writer, speed int, blob []byte) (int, time.Duration) {
+	sum := 0
+	totalSize := len(blob)
+	sendDuration := 100 * time.Millisecond
+	sendSize := speed / int(time.Second/sendDuration)
+	begin := time.Now()
+	for sendN := 0; sendN < totalSize; sendN += sendSize {
+		n, _ := w.Write(blob[:sendSize])
+
+		est := time.Millisecond * time.Duration(float64(sendN+sendSize)/float64(speed)*1000)
+		duration := est - time.Now().Sub(begin)
+		//fmt.Println(sendN+sendSize, speed, est, time.Now().Sub(begin), duration)
+		if duration > 0 {
+			time.Sleep(duration)
+		}
+		sum += n
+	}
+	spend := time.Now().Sub(begin)
+	//fmt.Println(sum, spend, float64(sum)/float64(speed))
+	return sum, spend
+}
+
+func multiSend(count int, writeMaker func() io.Writer, speed int, totalSize int) (int, time.Duration) {
+	wg := sync.WaitGroup{}
+
+	wg.Add(count)
+	blob := make([]byte, totalSize)
+
+	totalN := 0
+	begin := time.Now()
+	for i := 0; i < count; i++ {
+		go func() {
+			w := writeMaker()
+			// 确保原子操作
+			n, _ := send(w, speed, blob)
+			totalN += n
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	spend := time.Now().Sub(begin)
+	return totalN, spend
 }
